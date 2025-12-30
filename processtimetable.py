d944d2bb57e0ffa0aeb0fc1df9b2309b58532da8
Dheeraj36229 <gargd9245@gmail.com>
2025-12-31 00:28:57 +0530

    Update processtimetable.py

--- processtimetable.py
+++ processtimetable.py
@@ -13,6 +13,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 FILE_NAME = "user_settings.txt"
 USERNAME = "xceed_timetable"  
