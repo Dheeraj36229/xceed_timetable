211cfc840ebb2bb5e7a80699f57ec49a5e26f703
Dheeraj36229 <gargd9245@gmail.com>
2025-12-31 00:15:20 +0530

    Update processtimetable.py

--- processtimetable.py
+++ processtimetable.py
@@ -101,7 +101,7 @@ def get_driver():
     options.add_argument("--window-size=1920,1080")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     options.add_argument("--disable-gpu")
-    return webdriver.Chrome(ChromeDriverManager().install()),options=options)
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 
 
 def process_timetables():
